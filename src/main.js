860519047bfe092babb0273d9674c5107a40890e
David Couronné <couronne.david@gmail.com>
2019-10-29 17:40:25 +0100

    Add tomorrow-dark theme

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // Import main css
-require("prismjs/themes/prism-solarizedlight.css")
+//require("prismjs/themes/prism-solarizedlight.css")
+require('prismjs/themes/prism-tomorrow.css');
 require("prismjs/plugins/line-numbers/prism-line-numbers.css")
 require("prismjs/plugins/command-line/prism-command-line.css")
 import '~/assets/style/index.scss'
