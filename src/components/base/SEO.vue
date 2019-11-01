d4676e648dacc5a9e97e6caa66c6cf804f8fddb9
David Couronné <couronne.david@gmail.com>
2019-11-01 10:57:35 +0100

    Update SEO.vue

--- src/components/base/SEO.vue
+++ src/components/base/SEO.vue
@@ -1,5 +1,3 @@
-
-
 <static-query>
 query {
   metadata {
@@ -11,25 +9,44 @@ query {
 
 <script>
 export default {
-    metaInfo (){
-        return {
-            meta: [
-        { name: 'description', content: this.$page.post.description },
+  metaInfo() {
+    return {
+      meta: [
+        { name: "description", content: this.$page.post.description },
 
+        // Some Open Graph Tags
         { property: "og:title", content: this.$page.post.title },
-        { property: "og:description", content: this.$page.post.description },        
+        { property: "og:description", content: this.$page.post.description },
         { property: "og:image", content: this.$page.post.cover_image },
-        { property: "og:url", content: this.$static.metadata.siteUrl+this.$page.post.path },
+        {
+          property: "og:url",
+          content: this.$static.metadata.siteUrl + this.$page.post.path
+        },
 
+        // Some Twiter Cards Tags
         { name: "twitter:card", content: "summary_large_image" },
         { name: "twitter:title", content: this.$page.post.title },
         { name: "twitter:image", content: this.$page.post.cover_image },
-        { name: "twitter:description", content: this.$page.post.description },
-        
-      ]
+        { name: "twitter:description", content: this.$page.post.description }
+      ],
+      //Somme ld+json tags
+      script: [
+        {
+          type: "application/ld+json",
+          json: {
+            "@context": "http://schema.org",
+            "@type": "BlogPosting",
+            description: this.$page.post.description,
+            datePublished: this.$page.post.date,
+            author: {
+              name: this.$page.post.author
+            },
+            headline: this.$page.post.title,
+            image: this.$page.post.cover_image,
+          }
         }
-
-    }
-    
-}
+      ]
+    };
+  }
+};
 </script>
